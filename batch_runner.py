cc1b5c01c196befc4568d3cdacea8445fa7da157
Hai HoDac <hhodac@student.unimelb.edu.au>
2020-11-04 11:25:29 +1100

    Modified single threads to multi-threading processing for batch_runner.py

--- batch_runner.py
+++ batch_runner.py
@@ -1,31 +1,61 @@
 from mesa.batchrunner import FixedBatchRunner
 from model import *
+import multiprocessing
 
-simulation_batch = FixedBatchRunner(
-    model_cls=HeterogeneityInArtificialMarket,
-    model_reporters={
-        "Price": get_market_price,
-        "FundamentalValue": get_market_value,
-        "Order": get_market_order,
-        "NetFundamentalPosition": get_fundamental_position,
-        "NetTechnicalPosition": get_technical_position
-    },
-    fixed_parameters={
+def fixed_batch_init():
+    """
+    Initializes and returns batch runner with fixed-parameters.
+    """
+    # Define fixed-parameters here
+    params = {
         "initial_fundamentalist": 100,
         "initial_technical": 100,
         "initial_mimetic": 0,
         "initial_noise": 0,
         "network_type": "small world"
-    },
-    max_steps=1800
-)
+    }
 
-iterations = 10
-for i in range(iterations):
-    print("Step {}".format(i))
+    # Model reporter (similar to data collector from model.py)
+    model_reporters = {
+        "Price": get_market_price,
+        "FundamentalValue": get_market_value,
+        "Order": get_market_order,
+        "NetFundamentalPosition": get_fundamental_position,
+        "NetTechnicalPosition": get_technical_position
+    }
+
+    # Define number of maximum iterations
+    MAX_ITERATIONS = 1800
+
+    # Generate fixed batch runner object
+    batch = FixedBatchRunner(
+        model_cls=HeterogeneityInArtificialMarket,
+        model_reporters=model_reporters,
+        fixed_parameters=params,
+        max_steps=MAX_ITERATIONS
+    )
+    return batch
+
+def run_simulation(i):
+    print("Iteration {} running...".format(i))
+    simulation_batch = fixed_batch_init()
     model = HeterogeneityInArtificialMarket()
     simulation_batch.run_model(model)
     df = model.datacollector.get_model_vars_dataframe()
     file_name = "batch_record_"+str(i)+".csv"
     df.to_csv(file_name, header=True, index=False)
+    print("Iteration {} completed.".format(i))
+
+# Activate multiprocessing
+print("Start multiprocessing...")
+
+optimal_thread_count = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(optimal_thread_count)
+
+iterations = 10
+pool.map(run_simulation, list(range(iterations)))
+
+pool.close()
+pool.join()
+
 print("Completed!")
