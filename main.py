061e5f2f0ee04fd571d175d37ca6841ed3e239db
mpriymak <mpriymak@gmail.com>
2020-11-04 22:37:53 +1100

    fixed bug in model_reporter

--- main.py
+++ main.py
@@ -26,21 +26,21 @@ def fixed_batch_init():
         "net_orders": lambda m: m.get_market_parameters(param_name='order'),
 
         "pos_ftrader_mean": lambda m: m.get_agent_stats(trader_type='fundamental', param_name='position', stats_type='mean'),
-        "pos_ttrader_mean": lambda m: m.get_agent_stats(trader_type='fundamental', param_name='position', stats_type='mean'),
-        "pos_mtrader_mean": lambda m: m.get_agent_stats(trader_type='fundamental', param_name='position', stats_type='mean'),
-        "pos_ntrader_mean": lambda m: m.get_agent_stats(trader_type='fundamental', param_name='position', stats_type='mean'),
-        "pos_all_mean": lambda m: m.get_agent_stats(trader_type='fundamental', param_name='position', stats_type='mean')
+        "pos_ttrader_mean": lambda m: m.get_agent_stats(trader_type='technical', param_name='position', stats_type='mean'),
+        "pos_mtrader_mean": lambda m: m.get_agent_stats(trader_type='mimetic', param_name='position', stats_type='mean'),
+        "pos_ntrader_mean": lambda m: m.get_agent_stats(trader_type='noise', param_name='position', stats_type='mean'),
+        "pos_all_mean": lambda m: m.get_agent_stats(trader_type='all', param_name='position', stats_type='mean')
     }
 
     # Define number of maximum iterations
-    max_iterations = 1530
+    max_steps = 100
 
     # Generate fixed batch runner object
     batch = FixedBatchRunner(
         model_cls=HeterogeneityInArtificialMarket,
         model_reporters=model_reporters,
         fixed_parameters=params,
-        max_steps=max_iterations
+        max_steps=max_steps
     )
 
     return batch
